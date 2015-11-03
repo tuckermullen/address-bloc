5a9cdba7130cdd6192d4af104e60cb0e56731721
tuckermullen <mullentucker@gmail.com>
2015-11-03 14:06:54 -0800

    fixed a failing test

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -124,7 +124,7 @@ RSpec.describe AddressBook do
 
         it "searches AddressBook for Bob" do
           book.import_from_csv("entries.csv")
-          entry = book.binary_search("Bill")
+          entry = book.binary_search("Bob")
           expect(entry).to be_a Entry
           check_entry(entry, "Bob", "555-555-5415", "bob@blocmail.com")
         end
